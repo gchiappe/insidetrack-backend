64c332021729a3ab3e17a9a0aa732ab7b3044592
Giancarlo A. Chiappe <gchiappe@outlook.com.pe>
2021-12-06 05:42:12 -0500

    added master elements query resolvers

--- src/main/java/com/techstart/insidetracktest/graphql/ITTQueryResolver.java
+++ src/main/java/com/techstart/insidetracktest/graphql/ITTQueryResolver.java
@@ -1,10 +1,11 @@
 package com.techstart.insidetracktest.graphql;
 
 import com.techstart.insidetracktest.data.arangodb.InvoiceRepository;
-import com.techstart.insidetracktest.data.graphql.Invoice;
+import com.techstart.insidetracktest.data.graphql.*;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Component
@@ -35,5 +36,46 @@ public class ITTQueryResolver implements GraphQLQueryResolver {
     public Iterable<Invoice> invoicesByProduct(String productCode) {
         return this.invoiceRepository.findInvoicesByProduct_Code(productCode);
     }
+
+    public Iterable<Distributor> distributors() {
+        HashMap<String, Distributor> distributorHashMap = new HashMap<>();
+        Iterable<Invoice> invoiceIterable = this.invoiceRepository.findAll();
+        for ( Invoice invoice : invoiceIterable ) {
+            Distributor distributor = invoice.getDistributor();
+            if (!distributorHashMap.containsKey(distributor.getName())) {
+                distributorHashMap.put(distributor.getName(), distributor);
+            }
+        }
+        return distributorHashMap.values();
+    }
+
+    public Iterable<Customer> customers() {
+        HashMap<String, Customer> customersHashMap = new HashMap<>();
+        Iterable<Invoice> invoiceIterable = this.invoiceRepository.findAll();
+
+        for ( Invoice invoice : invoiceIterable ) {
+            Customer customer = invoice.getCustomer();
+            if (!customersHashMap.containsKey(customer.getName())) {
+                customersHashMap.put(customer.getName(), customer);
+            }
+        }
+        return customersHashMap.values();
+    }
+
+    public Iterable<Product> products() {
+        HashMap<String, Product> productHashMap = new HashMap<>();
+        Iterable<Invoice> invoiceIterable = this.invoiceRepository.findAll();
+        for ( Invoice invoice : invoiceIterable ) {
+            for (InvoiceDetail invoiceDetail : invoice.getDetails()) {
+                Product product = invoiceDetail.getProduct();
+                if (!productHashMap.containsKey(product.getCode())) {
+                    productHashMap.put(product.getCode(), product);
+                }
+            }
+        }
+        return productHashMap.values();
+    }
+
+
 
 }
